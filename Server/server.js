22c4f045d5eb192fbf94d1a46f9a940efedec67b
Suraj Vijay <surajcpp115@gmail.com>
2021-11-25 07:06:42 +0530

    added get request to base address

--- Server/server.js
+++ Server/server.js
@@ -22,6 +22,10 @@ app.use("/conversation", conversationRoute);
 app.use("/message", messageRoute);
 app.use("/interest", interestRoute);
 
+app.get("/", (req, res) => {
+    console.log(res.status(200).json("Base route"));
+});
+
 const PORT = process.env.port || 5000;
 app.listen(PORT, async () => {
     console.clear();
