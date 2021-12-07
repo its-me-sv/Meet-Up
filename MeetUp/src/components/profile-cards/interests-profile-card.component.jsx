5c2429c663a0e4c9dad4623f185e347395b2e983
Suraj Vijay <surajcpp115@gmail.com>
2021-12-07 12:05:16 +0530

    fetching interest details from server

--- MeetUp/src/components/profile-cards/interests-profile-card.component.jsx
+++ MeetUp/src/components/profile-cards/interests-profile-card.component.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import axios from "axios";
+import { useIsFocused } from "@react-navigation/native";
+
+import Loader from "../loader/loader.component";
 
 const InterestProfileCard = ({ route }) => {
     const { id } = route.params;
+    const [interest, setInterest] = useState(null);
+    const [people, setPeople] = useState([]);
+    const isFocused = useIsFocused();
+    useEffect(() => {
+        axios.get(`http://192.168.29.97:5000/interest/origin/${id}`)
+        .then(({data}) => setInterest(data))
+        .catch(console.log);
+        axios.get(`http://192.168.29.97:5000/interest/people/${id}`)
+        .then(({data}) => setPeople(data))
+        .catch(console.log);
+    }, [isFocused, id]);
+    if (!interest) return <Loader />;
+    console.log(people);
     return (
         <View style={{
             flex: 1,
