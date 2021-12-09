d78a128bb0ce15492a890587d08af2180be04b87
Suraj Vijay <surajcpp115@gmail.com>
2021-12-09 22:12:41 +0530

    interest card done

--- MeetUp/src/components/profile-cards/interests-profile-card.component.jsx
+++ MeetUp/src/components/profile-cards/interests-profile-card.component.jsx
@@ -1,9 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 import axios from "axios";
 import { useIsFocused } from "@react-navigation/native";
+import { Divider } from "react-native-paper";
+import { format } from "timeago.js";
+import numberToText from 'number-to-text';
 
 import Loader from "../loader/loader.component";
+import {
+    AccountContainer,
+    JoinedContainer,
+    TextVariant2,
+    TextVariant3,
+    RightEndText
+} from "../../features/settings/screens/view-account.styles";
+import Spacer from "../spacer/spacer.component";
+import PersonCard from "../../features/settings/components/person-card.component";
+
+const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+const formatTime = date => {
+    let newDate = new Date(date);
+    let day = newDate.getDate();
+    let month = months[newDate.getMonth()];
+    let year = newDate.getFullYear();
+    return `${month} ${day}, ${year}`;
+};
 
 const InterestProfileCard = ({ route }) => {
     const { id } = route.params;
@@ -20,11 +45,60 @@ const InterestProfileCard = ({ route }) => {
     }, [isFocused, id]);
     if (!interest) return <Loader />;
     return (
-        <View style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center"
-        }}><Text>{id}</Text></View>
+        <ScrollView>
+            <AccountContainer>
+                <Text style={{
+                    alignSelf: "center", 
+                    marginTop: 21, 
+                    fontSize: 28,
+                    color: "#373434"
+                }}>{interest.name}</Text>
+                <Spacer size="large"/>
+                <Spacer size="large"/>
+                <Divider />
+                <Spacer size="medium" />
+                <JoinedContainer>
+                    <View>
+                        <TextVariant2>Created</TextVariant2>
+                        <TextVariant3>{formatTime(interest.createdAt)}</TextVariant3>
+                    </View>
+                    <RightEndText>{format(interest.createdAt)}</RightEndText>
+                </JoinedContainer>
+                <Spacer size="medium" />
+                <Divider />
+                <Spacer size="medium" />
+                <JoinedContainer>
+                    <View>
+                        <TextVariant2>Followed by</TextVariant2>
+                        <TextVariant3>{people.length}</TextVariant3>
+                    </View>
+                    <RightEndText>{numberToText.convertToText(people.length)} people</RightEndText>
+                </JoinedContainer>
+                <Spacer size="medium" />
+                <Divider />
+                <Spacer size="medium" />
+                <View>
+                    <TextVariant2>Followers</TextVariant2>
+                    <Spacer size="medium" />
+                    {!people.length && <TextVariant3 opaque={true}>No followers yet</TextVariant3>}
+                    <ScrollView>
+                    {
+                        people.map(({_id, username, profilePicture: pp, email: friendEmail}) => {
+                            return (
+                                <PersonCard 
+                                    key={_id}
+                                    id={_id}
+                                    picture={pp}
+                                    username={username}
+                                    email={friendEmail}
+                                />
+                            );
+                        })
+                    }
+                    </ScrollView>
+                </View>
+            </AccountContainer>
+        </ScrollView>
     );
 };
 
