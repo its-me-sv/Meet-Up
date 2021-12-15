8d1d0ecf2433aa46c7b1e9cac3b56d18594412e1
Suraj Vijay <surajcpp115@gmail.com>
2021-12-15 05:42:51 +0530

    added text input for messages

--- MeetUp/src/features/chat/screens/messages.screen.jsx
+++ MeetUp/src/features/chat/screens/messages.screen.jsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, TextInput } from "react-native";
 import axios from "axios";
+import { Searchbar } from 'react-native-paper';
+import styled from "styled-components/native";
 
 import Loader from "../../../components/loader/loader.component";
 import Message from "../components/message.component";
+
+const MessageSender = styled(TextInput)`
+    z-index: 999;
+    bottom: 3px;
+    border-radius: 21px;
+    font-size: 18px;
+    width: 80%;
+    background-color: #ffffff;
+    padding: 7px;
+    border-color: #575656;
+    border-width: 2px;
+    margin: 3px;
+    width: 98%;
+    align-self: center;
+`;
 
 const MessagesScreen = ({ navigation, route }) => {
     const { person, convoId, userId} = route.params;
@@ -16,19 +33,24 @@ const MessagesScreen = ({ navigation, route }) => {
     }, []);
     if (messages === null) return <Loader />;
     return (
-        <ScrollView>
-            {
-                [...messages, ...messages, ...messages].map(({_id, sender, text, createdAt}) => (
-                    <Message 
-                        key={_id}
-                        sender={sender}
-                        message={text}
-                        time={createdAt}
-                        userId={userId}
-                    />
-                ))
-            }
-        </ScrollView>
+        <>
+            <ScrollView>
+                {
+                    [...messages, ...messages, ...messages].map(({_id, sender, text, createdAt}) => (
+                        <Message 
+                            key={_id}
+                            sender={sender}
+                            message={text}
+                            time={createdAt}
+                            userId={userId}
+                        />
+                    ))
+                }
+            </ScrollView>
+            <MessageSender
+                placeholder="Message..."
+            />
+        </>
     );
 };
 
